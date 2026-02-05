6b602eb144d98633d2ba7e6f36ef1b3e4a96839b
이수하 <swuhalee@gmail.com>
2026-02-05 14:20:59 +0900

    feat: db 배포 세팅

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
-
+require('dotenv').config();
 const app = express();
+const { MONGODB_URI_PROD } = process.env;
+
 var whitelist = ['http://localhost:3000'] 
 var corsOptions = {  
       origin: function (origin, callback) {
@@ -18,7 +20,7 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use('/api', indexRouter);
 
-const mongoURI = 'mongodb://localhost:27017/todo-demo';
+const mongoURI = MONGODB_URI_PROD;
 // 최신 버전 db 드라이브에선 useNewUrlParser, useUnifiedTopology 옵션이
 // 기본값으로 설정되어 있어서 별도 지정이 불필요함
 mongoose.connect(mongoURI)
