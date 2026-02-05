996b35c706c516c32b576fed0ebdc6e8b5be013b
이수하 <swuhalee@gmail.com>
2026-02-05 17:31:38 +0900

    fix: app.js 코드 순서 수정

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const indexRouter = require('./routes/index');
 require('dotenv').config();
+
+const indexRouter = require('./routes/index');
 const app = express();
 const { MONGODB_URI_PROD } = process.env;
 
-app.use(cors);
+app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.status(200).send('OK');
@@ -18,12 +19,10 @@ app.get('/', (req, res) => {
 app.use('/api', indexRouter);
 
 const mongoURI = MONGODB_URI_PROD;
-// 최신 버전 db 드라이브에선 useNewUrlParser, useUnifiedTopology 옵션이
-// 기본값으로 설정되어 있어서 별도 지정이 불필요함
 mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
 app.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080');
-});    
+    console.log('Server is running on port 8080');
+});
